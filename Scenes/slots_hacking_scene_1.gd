f1cd5434cf377a4931cee87373004da182287787
Austyn-Reichback <alreic03@louisville.edu>
2024-10-25 17:31:19 -0400

    added backgrounds
    Added backgrounds, so it was not just green.

--- Scenes/slots_hacking_scene_1.gd
+++ Scenes/slots_hacking_scene_1.gd
@@ -2,14 +2,14 @@ extends Node2D
 
 #creates variable for animation player
 @onready var AP = $AnimationPlayer
-var position = 53
+var Yposition = 53
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	 #when this part starts, the right block will move
-	while(!_on_stop_button_pressed()):
-		if (position == 350):
-			
+	#while(!_on_stop_button_pressed()):
+		#if (Yposition == 350):
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -18,4 +18,5 @@ func _process(delta: float) -> void:
 
 func _on_stop_button_pressed() -> void:
 	
-	return true
+	#return true
+	pass
