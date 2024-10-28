e22934bec7a37118e27ebdf2aa02f2abcc3caea7
Austyn-Reichback <alreic03@louisville.edu>
2024-10-27 20:25:49 -0400

    finished hacking event
    Hacking event will, if won, double the bet and send you to the slots minigame. If you lose, you lose all of the bet and get sent back to slots menu. Can change if needed and whenever bet menu is available.

--- Scenes/slots_hacking_scene_1.gd
+++ Scenes/slots_hacking_scene_1.gd
@@ -2,14 +2,12 @@ extends Node2D
 
 #creates variable for animation player
 @onready var AP = $AnimationPlayer
-var Yposition = 53
+var Xposition = 956
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	 #when this part starts, the right block will move
-	#while(!_on_stop_button_pressed()):
-		#if (Yposition == 350):
-	pass
+	AP.play("moving_block")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -17,6 +15,25 @@ func _process(delta: float) -> void:
 	
 
 func _on_stop_button_pressed() -> void:
-	
-	#return true
-	pass
+	#stops up and down animation
+	AP.stop(true)
+	#moves moving block over to the input block
+	while(Xposition >= 492):
+		Xposition = Xposition - 1;
+		$Polygon_moving.position.x = Xposition;
+		await get_tree().create_timer(.0001).timeout
+	#Checks to see if within area in terms of Y axis
+	if ($Polygon_moving.position.y >= 140 and $Polygon_moving.position.y<=165):
+		#if it is within, the stop button is taken away, a win label explaining
+		#that the person won and sends them to the slots minigame
+		$"win_Label".visible = true
+		$stop_Button.visible = false
+		await get_tree().create_timer(1.5).timeout
+		get_tree().change_scene_to_file("res://Scenes/slots_game.tscn")
+	else:
+		#if it is not within, the stop button is taken away, a lose label explaining
+		#that the person lost and sends them to the slots menu to bet again
+		$"lose_label".visible = true
+		$stop_Button.visible = false
+		await get_tree().create_timer(1.5).timeout
+		get_tree().change_scene_to_file("res://Scenes/slots_menu.tscn")
